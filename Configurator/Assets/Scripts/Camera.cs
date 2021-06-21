9d0572957d65d2226f4aa0193e577c3b2e15c49c
Exiolite <kirkirlvov@gmail.com>
2021-06-21 14:59:35 +0300

    0.0.5 AttachmentPointsUi

---
+++ Configurator/Assets/Scripts/Camera.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+        public static Transform Transform { get; private set; }
+
+        private void Awake() => Transform = GetComponent<Transform>();
+}
---
+++ Configurator/Assets/Scripts/Configurator/AttachmentPointUi.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Configurator
+{
+    [RequireComponent(typeof(Transform))]
+    public class AttachmentPointUi : MonoBehaviour
+    {
+        [SerializeField] private bool _isEnabled;
+        [SerializeField] private Transform _transform;
+
+
+        private void Awake() => _transform.gameObject.SetActive(_isEnabled);
+
+        private void LateUpdate()
+        {
+            _transform.LookAt(Camera.Transform);
+            var distanceToCamera = Vector3.Distance(_transform.position, Camera.Transform.position)/10;
+            _transform.localScale = new Vector3(distanceToCamera,distanceToCamera,distanceToCamera);
+        }
+    }
+}
